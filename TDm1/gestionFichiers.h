1a57f3143a8717ca645b25997def5004d54fdebe
RaphBhz <raphael.bouchez@etu.u-paris.fr>
2024-05-05 18:59:20 +0200

    fix comments in gestionFichier

--- TDm1/gestionFichiers.h
+++ TDm1/gestionFichiers.h
@@ -1,19 +1,25 @@
 #ifndef _GESTION_FICHIERS_H
 #define _GESTION_FICHIERS_H
 
-/// @brief Lit dix caractères du fichier dont le descripteur est fd.
-/// @param  fd Le descripteur du fichier à lire.
-/// @return Un pointeur vers une chaîne de caractères de dix caractères lus.
+/*
+    @brief Lit dix caractères du fichier dont le descripteur est fd.
+    @param  fd Le descripteur du fichier à lire.
+    @return Un pointeur vers une chaîne de caractères de dix caractères lus.
+*/
 char* litDixCaracteres(int);
 
-/// @brief Lit une ligne du fichier dont le descripteur est fd.
-/// @param  fd Le descripteur du fichier à lire.
-/// @return Un pointeur vers une chaîne de caractères contenant la ligne lue.
+/*
+    @brief Lit une ligne du fichier dont le descripteur est fd.
+    @param  fd Le descripteur du fichier à lire.
+    @return Un pointeur vers une chaîne de caractères contenant la ligne lue.
+*/
 char* litLigne(int);
 
-/// @brief Écrit la chaîne de caractères s dans le fichier dont le descripteur est fd.
-/// @param  fd Le descripteur du fichier à écrire.
-/// @param  s  La chaîne de caractères à écrire.
+/*
+    @brief Écrit la chaîne de caractères s dans le fichier dont le descripteur est fd.
+    @param  fd Le descripteur du fichier à écrire.
+    @param  s  La chaîne de caractères à écrire.
+*/
 void ecrire(int, char*);
 
 #endif
