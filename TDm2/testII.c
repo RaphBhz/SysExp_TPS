66c911103ccc8a99c2c14510b0e7cac4fdf86daf
RaphBhz <raphael.bouchez@edu.esiee.fr>
2024-05-05 18:57:55 +0200

    TDm2

--- TDm2/testII.c
+++ TDm2/testII.c
@@ -1,26 +1,27 @@
 #include "gestionProcess.h"
 
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 int main()
 {
-    pid_t pid = execv("/mnt/d/pers/esiee/E4FI/Systeme-Exploitation/SysExp_TPS/TDm2", []);
+    pid_t pid = fork();
 
     if (pid == 0)
     {
         sleep(1);
-        print_pid_ppid("Fils");
+        /* On éxecute le fichier exécutable du source testIB1.c pour recouvrir le processus fils. */
+        execl("./testIB1", "./testIB1", (char*) NULL);
     } else if (pid > 0)
     {
         /* On attend que le processus fils finisse avant de compléter l'exécution du processus père. */
         /* Tant que waitpid ne renvoie pas la valeur de l'id du processus fils, celui-ci n'est pas terminé. */
         while (waitpid(pid, NULL, WNOHANG) != pid);
-        print_pid_ppid("Je suis le père");
+        print_pid_ppid("Je suis le Père");
     } else 
     {
         perror("Erreur : Fork");
